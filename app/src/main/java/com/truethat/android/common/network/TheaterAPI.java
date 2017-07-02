f9e5094abc010da81b70142e3040c34a856b429d
ohad <ohadnav@gmail.com>
2017-07-02 12:55:44 +0300

    javadoc

--- app/src/main/java/com/truethat/android/common/network/TheaterAPI.java
+++ app/src/main/java/com/truethat/android/common/network/TheaterAPI.java
@@ -2,12 +2,14 @@ package com.truethat.android.common.network;
 
 import com.truethat.android.model.Reactable;
 import com.truethat.android.model.ReactableEvent;
+import com.truethat.android.model.User;
 import java.util.List;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 /**
  * Proudly created by ohad on 01/06/2017 for TrueThat.
@@ -17,11 +19,12 @@ import retrofit2.http.POST;
 
 public interface TheaterAPI {
   String PATH = "theater";
+  String USER_PARAM = "user";
 
   /**
    * Get reactables from out beloved backend to add some drama to our users life.
    */
-  @GET(PATH) Call<List<Reactable>> getReactables();
+  @GET(PATH) Call<List<Reactable>> getReactables(@Query(USER_PARAM) User user);
 
   /**
    * Informs our backend of the the current user interaction with reactables.
